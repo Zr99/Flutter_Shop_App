878289bea400f81d4cd11e1f2f456b5c44eb1f8f
Ryan Ling <Ryan.Ling@metalform.co.nz>
2022-03-24 11:12:18 +1300

    Added the fonts

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,8 +9,31 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageURL!),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageURL!,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            title!,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 }
